138180f2b4283731e19f9fbb550257d57642b255
JonathanVVMM <JonathanVillaplanaMocholi@gmail.com>
2025-01-15 19:00:54 +0100

    Añado DATABASE Y MODELOS

---
+++ ProyectoFaltas/Database/Constants.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFaltas.Database
+{
+    public class Constants
+    {
+        // Nombre de la base de datos
+        public const string DatabaseFilename = "Faltas.db3";
+
+        public const SQLite.SQLiteOpenFlags Flags =
+            // Acceso de lectura y escritura a la base de datos
+            SQLite.SQLiteOpenFlags.ReadWrite |
+            // Crea la base de datos si no existe
+            SQLite.SQLiteOpenFlags.Create |
+            // Creamos el acceso multihilo a la base de datos
+            SQLite.SQLiteOpenFlags.SharedCache;
+
+        // Cadena con la ruta a la base de datos, directorio y nombre del fichero
+        public static string DatabasePath =
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), DatabaseFilename);
+    }
+}
---
+++ ProyectoFaltas/Database/DatabaseService.cs
@@ -0,0 +1,37 @@
+﻿using ProyectoFaltas.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFaltas.Database
+{
+    public class DatabaseService
+    {
+
+        private SQLiteAsyncConnection Database;
+
+        public DatabaseService() { }
+
+        private async Task Init() //Metodo para crear la conexion con la base de datos, TODOS LOS METODOS QUE HAGAN ALGO SOBRE LA BASE DE DATOS DEBEN DE ESPERAR A ESTE
+        {
+            if (Database is not null) return;
+            Database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags); //Si ha llegado hasta aqui, es porque no está conectado a la base de datos y la crea o se conecta a ella
+            await Database.CreateTableAsync<Activo>();
+            await Database.CreateTableAsync<Curso>();
+            await Database.CreateTableAsync<Falta>();
+            await Database.CreateTableAsync<Festivo>();
+            await Database.CreateTableAsync<Profesor>();
+            await Database.CreateTableAsync<TipoFalta>();
+        }
+
+        //TABLA ACTIVO
+        public async Task<int> AddActivoAsync(Activo activo)
+        {
+            return await Database.InsertAsync(activo);
+        }
+        public Task
+    }
+}
