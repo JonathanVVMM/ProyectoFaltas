511a4e3a6065b7156ace54054b139e2c4c20439a
JonathanVVMM <JonathanVillaplanaMocholi@gmail.com>
2025-01-15 18:21:41 +0100

    Añado Curso y Festivo

---
+++ ProyectoFaltas/Models/Festivo.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFaltas.Models
+{
+    public class Festivo
+    {
+        [PrimaryKey, AutoIncrement]
+        public long Id { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public string Tipo { get; set; } // VARCHAR(10)
+
+        [Indexed]
+        public long IdCursos { get; set; }
+    }
+}
