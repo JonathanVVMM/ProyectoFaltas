f3ba6a7f36c543aee6bace43edd15bf9f3a5770c
Unai <unamarizq@gmail.com>
2025-01-15 18:44:34 +0100

    comentando en cuurso.cs

--- ProyectoFaltas/Models/Curso.cs
+++ ProyectoFaltas/Models/Curso.cs
@@ -10,6 +10,6 @@ namespace ProyectoFaltas.Models
     public class Curso
     {
         [PrimaryKey, AutoIncrement]
-        public long Id { get; set; }
+        public long Id { get; set; } //Hola que ase
     }
 }
