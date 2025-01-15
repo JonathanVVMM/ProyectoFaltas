7b278f0f09ee7d79b5cea7e1670668732cef0bd7
JonathanVVMM <JonathanVillaplanaMocholi@gmail.com>
2025-01-15 18:19:16 +0100

    Añadido el modelo Activo

---
+++ ProyectoFaltas/Models/Activo.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ProyectoFaltas.Models
+{
+    public class Activo
+    {
+        [PrimaryKey, AutoIncrement]
+        public long Id { get; set; }
+
+        [Indexed]
+        public long IdCursos { get; set; }
+
+        [Indexed]
+        public long IdProfesores { get; set; }
+    }
+}
