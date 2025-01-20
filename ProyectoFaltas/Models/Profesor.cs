c91229ced9ff8c6e2d1932ffb53cffcf6ab177cb
JonathanVVMM <JonathanVillaplanaMocholi@gmail.com>
2025-01-20 16:55:43 +0100

    FUNCIONA 2

--- ProyectoFaltas/Models/Profesor.cs
+++ ProyectoFaltas/Models/Profesor.cs
@@ -1,21 +1,64 @@
 ﻿using SQLite;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ProyectoFaltas.Models
 {
-    public class Profesor
+    public class Profesor : INotifyPropertyChanged
     {
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        private int _id;
+        private string _nombre;
+        private string _apellidos;
+        private string _tipo;
+
         [PrimaryKey, AutoIncrement]
-        public long Id { get; set; }
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                _id = value;
+            }
+        }
 
-        public string Nombre { get; set; } // VARCHAR(50)
+        public string Nombre
+        {
+            get => _nombre;
+            set
+            {
+                _nombre = value; OnPropertyChanged();
+            }
+        }
 
-        public string Apellidos { get; set; } // VARCHAR(100)
+        public string Apellidos
+        {
+            get => _apellidos;
+            set
+            {
+                _apellidos = value; OnPropertyChanged();
+            }
+        }
 
-        public string Tipo { get; set; } // ENUM(...) (puedes ajustarlo según tus necesidades)
+        public string Tipo
+        {
+            get => _tipo;
+            set
+            {
+                _tipo = value; OnPropertyChanged();
+            }
+        }
     }
 }
