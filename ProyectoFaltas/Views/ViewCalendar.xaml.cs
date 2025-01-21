7f529539d20ae3731f055ed64d8cf32b9bcb906d
JonathanVVMM <JonathanVillaplanaMocholi@gmail.com>
2025-01-21 18:38:02 +0100

    Implementamos visualmente el calendario pero falta configurarlo: - Claudia Tab - Cuando desseleccionas no se quita de la vista

--- ProyectoFaltas/Views/ViewCalendar.xaml.cs
+++ ProyectoFaltas/Views/ViewCalendar.xaml.cs
@@ -1,9 +1,74 @@
-namespace ProyectoFaltas.Views;
+using Plugin.Maui.Calendar.Models;
+using ProyectoFaltas.Models;
+using Microsoft.Maui.Controls;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Input;
 
-public partial class ViewCalendar : ContentPage
+namespace ProyectoFaltas.Views
 {
-	public ViewCalendar()
-	{
-		InitializeComponent();
+    public partial class ViewCalendar : ContentPage, INotifyPropertyChanged
+    {
+        public ObservableCollection<Event> Events { get; set; }
+        private Event _selectedEvent;
+
+        public Event SelectedEvent
+        {
+            get => _selectedEvent;
+            set
+            {
+                if (_selectedEvent != value)
+                {
+                    _selectedEvent = value;
+                    OnPropertyChanged(nameof(SelectedEvent));
+                    OnPropertyChanged(nameof(IsEventSelected));
+                }
+            }
+        }
+
+        public ICommand DayTappedCommand { get; }
+
+        public ViewCalendar()
+        {
+            InitializeComponent();
+            Events = new ObservableCollection<Event>
+            {
+                new Event { Name = "Evento 1", Description = "Descripción del Evento 1", Date = new DateTime(2025, 1, 21) },
+                new Event { Name = "Evento 2", Description = "Descripción del Evento 2", Date = new DateTime(2025, 1, 21) }
+            };
+            DayTappedCommand = new Command<DateTime>(OnDayTapped);  // Enlazamos el comando
+            BindingContext = this; // Set the BindingContext
+        }
+
+        private void OnDayTapped(DateTime selectedDate)
+        {
+            // Filtrar eventos según la fecha seleccionada
+            SelectedEvent = Events.FirstOrDefault(evento => evento.Date.Date == selectedDate);
+
+            // Si no hay evento en esa fecha, se puede limpiar el evento seleccionado
+            if (SelectedEvent == null)
+            {
+                SelectedEvent = null;
+            }
+        }
+
+        public bool IsEventSelected => SelectedEvent != null;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public class Event
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public DateTime Date { get; set; } // Nueva propiedad Date
+        }
     }
 }
+
+//agregar a la clase event una observable collection profesores para mostrar cuando elijamos un dia la lista de profesores que han faltado
